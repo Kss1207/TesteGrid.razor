3a883e35236880d176b232129f554ec742c50b44
kss1207 <kspaulista123@gmail.com>
2025-08-19 18:47:49 -0300

    alterações

--- Testegrid.razor/Program.cs
+++ Testegrid.razor/Program.cs
@@ -2,16 +2,12 @@ using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-
-// Adiciona serviços do MudBlazor
 builder.Services.AddMudServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
